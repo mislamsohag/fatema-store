73ce5d8ce17a5274b66704b42c2f1a91a5b6a34e
mislamsohag <mislamsohag@gmail.com>
2024-01-04 01:46:07 +0600

    password reset successfully

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Helper\JWTToken;
 use Exception;
 use App\Models\User;
+use App\Mail\OTPMail;
+use App\Helper\JWTToken;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UsersController extends Controller
 {
@@ -35,25 +36,98 @@ class UsersController extends Controller
 
     }
 
+
     function UserLogin(Request $request)
     {
         $count = User::where('email', '=', $request->input('email'))
             ->where('password', '=', $request->input('password'))
             ->count();
-// dd($count);
-            if($count==1){
-                $token=JWTToken::CreateToken($request->input('email'));
-                return response()->json([
-                    'status'=>'success',
-                    'message'=>'Login success',
-                    'token'=>$token
-                ]);
-            }else{
-                return response()->json([
-                    'status'=>'failed',
-                    'message'=>"Unauthorized"
-                ]);
-            }
+        // dd($count);
+        if ($count == 1) {
+            $token = JWTToken::CreateToken($request->input('email'));
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Login success',
+                'token' => $token
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                'message' => "Unauthorized"
+            ]);
+        }
     }
 
+
+    function SendOTPCODE(Request $request)
+    {
+        $email = $request->input('email'); //catch email
+        $otp = rand(1000, 9999); // make 4 digit random OTP
+
+        // find user matching request email
+        $count = User::where('email', '=', $email)->count();
+
+        if ($count != 1) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'unauthorized'
+            ]);
+        } else {
+            // OTP send
+            Mail::to($email)->send(new OTPMail($otp));
+            // OTP Update on Database by User Model
+            User::where('email', '=', $email)->update(['otp' => $otp]);
+            return response()->json([
+                'status' => 'OTP Send Success',
+                'message' => '4 digit OTP send your email successfully'
+            ]);
+        }
+
+    }
+
+    function VerifyOTP(Request $request)
+    {
+        $email=$request->input('email');
+        $otp=$request->input('otp');
+
+        $count=User::where('email','=', $email)
+        ->where('otp', '=', $otp)
+        ->count();
+
+        if($count !=1){
+            return response()->json([
+                'status'=>'failed',
+                'message'=>'unauthorized'
+            ]);
+        }else{
+            // OTP Update
+            User::where('email', '=', $email)->update(['otp'=>'0']);
+            // Password Issue and Token Issue
+            $token=JWTToken::CreateTokenForSetPassword($request->input('email'));
+            return response()->json([
+                'status'=>'success',
+                'message'=>'OTP verification successfully',
+                'token'=>$token
+            ]);
+        }
+    }
+
+    function ResetPassword(Request $request){
+        try{
+            $email=$request->header('email');
+            $password=$request->input('password');
+
+            User::where('email', '=', $email)->update(['password'=>$password]);
+            return response()->json([
+                'status'=>'success',
+                'message'=>'User password reset successfully'
+            ]);            
+        }
+        catch(Exception $e){
+            return response()->json([
+                'status'=>'failed',
+                'message'=>'Request unauthorized'
+            ]);
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\TokenVeficationMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 
@@ -11,3 +12,9 @@ Route::get('/', function () {
 
 Route::post('/user-registration', [UsersController::class, 'UserRegistration']);
 Route::post('/user-login', [UsersController::class, 'UserLogin']);
+Route::post('/send-otp', [UsersController::class, 'SendOTPCode']);
+Route::post('/verify-otp', [UsersController::class, 'VerifyOTP']);
+// Token Verify with Middleware
+Route::post('/reset-password', [UsersController::class, 'ResetPassword'])
+->middleware([TokenVeficationMiddleware::class]);
+ 
